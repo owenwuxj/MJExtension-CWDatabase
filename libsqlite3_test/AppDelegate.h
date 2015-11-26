d77a309684b948a36f07779dc17607abd983fc10
chaoyue3116 <xalo@W.local>
2015-11-27 00:00:58 +0800

    Initial Commit

---
+++ libsqlite3_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  libsqlite3_test
+//
+//  Created by xalo on 15/11/27.
+//  Copyright (c) 2015年 c.w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ libsqlite3_testTests/libsqlite3_testTests.m
@@ -0,0 +1,40 @@
+//
+//  libsqlite3_testTests.m
+//  libsqlite3_testTests
+//
+//  Created by xalo on 15/11/27.
+//  Copyright (c) 2015年 c.w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface libsqlite3_testTests : XCTestCase
+
+@end
+
+@implementation libsqlite3_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
